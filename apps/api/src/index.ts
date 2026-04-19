79752fbd96193937ba6a3f38829515f39758a0a0
waqas <waqas.official2005@gmail.com>
2026-04-19 16:51:03 +0500

    feat: add express

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,15 @@
-console.log("hi there");
+import "dotenv/config";
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(3000, () => {
+  if (process.env.NODE_ENV == "development") {
+    console.log("server running on http://localhost:3000");
+    return;
+  }
+  console.log("server running sucessfully");
+});
